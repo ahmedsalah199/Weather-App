3bd78c08cec310f24748c28fc2ac179fd26462a4
ahmed ali <ahmedshaakka>
2022-06-18 19:05:21 +0200

    change connectTimeOut

--- lib/presentation/resources/constants_manager.dart
+++ lib/presentation/resources/constants_manager.dart
@@ -2,5 +2,5 @@ class AppConstants {
   static const String apiKey = 'ca9695d3ccef49eb8837825d50f9851d';
   static const String baseUrls = 'https://api.openweathermap.org/data/2.5/';
   static const String baseUrl = 'https://api.weatherbit.io/v2.0/';
-  static const int connectTimeOut = 2000;
+  static const int connectTimeOut = 20000;
 }
