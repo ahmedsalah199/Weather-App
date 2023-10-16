4a13858caf631ef3979dbc327a624f10bf3bbbd4
ahmed ali <ahmedshaakka>
2023-10-16 16:16:22 +0400

    change api key

--- lib/presentation/resources/constants_manager.dart
+++ lib/presentation/resources/constants_manager.dart
@@ -1,5 +1,6 @@
 class AppConstants {
-  static const String apiKey = 'ca9695d3ccef49eb8837825d50f9851d';
+  // static const String apiKey = 'ca9695d3ccef49eb8837825d50f9851d';
+  static const String apiKey = '1ea63eede49046478d85ef8926347401';
   static const String baseUrls = 'https://api.openweathermap.org/data/2.5/';
   static const String baseUrl = 'https://api.weatherbit.io/v2.0/';
   static const int connectTimeOut = 20000;
